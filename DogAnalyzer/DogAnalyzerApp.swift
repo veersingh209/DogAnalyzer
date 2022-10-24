3248d16b31b0f8cb3dc12716926edfdf771b6fa2
Veer Singh <veersingh209@icloud.com>
2022-10-24 14:47:30 -0700

    Initial Commit

---
+++ DogAnalyzer/DogAnalyzerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogAnalyzerApp.swift
+//  DogAnalyzer
+//
+//  Created by Veer Singh on 10/24/22.
+//
+
+import SwiftUI
+
+@main
+struct DogAnalyzerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
